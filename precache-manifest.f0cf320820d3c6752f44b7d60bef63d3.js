165cdd9c500d44562733c09a67ce153a85e6a973
Md-Mudassir <mdmudassirpro@gmail.com>
2020-05-07 03:23:52 +0530

    Updates

---
+++ precache-manifest.f0cf320820d3c6752f44b7d60bef63d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ce59e931f49003fab4df95dfaf15f1",
+    "url": "/design-components/index.html"
+  },
+  {
+    "revision": "3061bf06357400836e1d",
+    "url": "/design-components/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "f39960b89301907533a9",
+    "url": "/design-components/static/js/2.1648d753.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/design-components/static/js/2.1648d753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3061bf06357400836e1d",
+    "url": "/design-components/static/js/main.97d769b9.chunk.js"
+  },
+  {
+    "revision": "89be272d411d3bfc746b",
+    "url": "/design-components/static/js/runtime-main.4f32d27c.js"
+  }
+]);
